cabf596af328a43d99649c448b5c299825b7a8f1
wilknisoliveira <wilknisoliveira@gmail.com>
2023-08-25 16:12:36 -0300

    feat: creating the application

---
+++ backend/growncloseapi/src/main/java/com/grownclose/GrownCloseApiApplication.java
@@ -0,0 +1,13 @@
+package com.grownclose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrownCloseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrownCloseApiApplication.class, args);
+	}
+
+}
